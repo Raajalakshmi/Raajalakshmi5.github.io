6de999a93daf024404a54552913ccbeb80ce2d93
Raajilakshmi <rajalakshmiprincess.5@gmail.com>
2016-03-20 14:04:06 +0530

    raji.php

---
+++ indexx.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+body {
+	background-color: #CCC;
+}
+</style>
+</head>
+
+<body>
+<h1>RAJALAKSHMI K</h1>
+<hr />
+<p>32,Mens college road</p>
+<p> Kumbakonam</p>
+<h3>contact:</h3> 
+<p> rajalakshmiprincess.5@gmail.com</p>
+<p> 54511455.
+  </p>
+</p>
+<p>.</p>
+<h1>CARRIER OBJECTIVE</h1>
+<hr />
+<p>To ensure challenging position in a growing organisation where i would be able to utilize my capabilities to the best extend.</p>
+<h1>ACADEMIC CREDENTIALS</h1>
+<hr />
+<table width="617" height="402" border="1" cellpadding="0" cellspacing="0">
+  <tr>
+    <th scope="col">COURSE</th>
+    <th scope="col">COLLEGE/UNIVERSITY</th>
+    <th scope="col">YEAR</th>
+    <th scope="col">AGGREGATE</th>
+  </tr>
+  <tr>
+    <td>SECONDARY SCHOOL LEAVING CERTICATE(SSLC)</td>
+    <td>SRI MATHA MATRICULATION HIGHER SECONDARY SCHOOL</td>
+    <td>2013</td>
+    <td>94.3</td>
+  </tr>
+  <tr>
+    <td>HIGHER SECONDARY EXAMINATION</td>
+    <td>SRI MATHA MATRICULATION HIGHER SECONDARY SCHOOL</td>
+    <td>2015</td>
+    <td>91.8</td>
+  </tr>
+  <tr>
+    <td>B.TECH(Computer science engineering)</td>
+    <td>SASTRA UNIVERSITY</td>
+    <td>2016</td>
+    <td>92.6</td>
+  </tr>
+</table>
+<h1>SOFTWARE AND HARDWARE SKILLS</h1>
+<hr />
+<h3>
+<ul>
+  <li>PROGRAMMING LANGUAGES: C,CPP,JAVA,HTML,FILE MAKER.</li>
+  <li>SOFTWARE SKILLS:SQL,ADOBE PHOOSHOT .
+  </li>
+  <li>HARDWARE SKILLS:NETWORKING,ASSEMBLING OF PC.</li>
+</ul>
+<h1>PERSONAL SKILLS AND STRENGTHS</h1>
+<hr />
+<ul>
+  <li>HAVING GOOD LOGIC SKILLS</li>
+  <li>HAVING GOOD LEADERSHIP CAPABILITY</li>
+  <li>GOOD MOTIVATING POWER</li>
+  <li>HAVING THE TENDENCY OF SOLVING COMPUTER ORIENTED PROBLEMS QUICKLY</li>
+</ul>
+<h1>OTHER CURRICULAR ACTIVITIES</h1>
+<hr />
+
+<ul>
+  <li>WON THE MEDAL FOR WINNING IN NATIONAL LEVEL CARROM BOARD. </li>
+  <li>WON THE PRIZE FOR STATE LEVEL QUIZ COMPETITION.</li>
+  <li>DONE PAPER PRESENTATION IN JEPPIAR INSTITUIONS,VELLAMAL UNIVERSITY.</li>
+</ul>
+</body>
+</html>
